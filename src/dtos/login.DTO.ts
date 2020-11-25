58c3236b6f9fbc3ad23731bb65ea84d9c712f6a7
Brian Kuan <bkuan72@gmail.com>
2020-11-26 11:32:04 +1300

    rename the file to better reflect the schema function

--- src/dtos/login.DTO.ts
+++ src/dtos/login.DTO.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { LoginData, loginDTO_schema } from '../schemas/login.schema';
+import { LoginData, loginDTO_schema } from '../schemas/loginDTO.schema';
 import DTOGenerator from '../modules/ModelGenerator';
 import CommonFn from '../modules/CommonFnModule';
 
