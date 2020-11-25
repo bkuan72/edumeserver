9141eceac96ca6433959b95373b1fb566c146a04
Brian Kuan <bkuan72@gmail.com>
2020-11-26 11:31:00 +1300

    updated sqls for renamed table names

--- SQL scripts/selectData.sql
+++ SQL scripts/selectData.sql
@@ -1,7 +1,13 @@
 use testdb;
 
 select * from tokens;
-select * from blacklistTokens;
+select * from blacklist_tokens;
 select * from users;
-select * from userAccounts;
+select * from user_accounts;
 select * from users;
+select * from logs;
+
+drop table userAccounts;
+delete from logs;
+delete from users;
+
