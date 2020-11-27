947a5fe785dc12bfd7c9d4b218c04727f5be441e
Brian Kuan <bkuan72@gmail.com>
2020-11-27 23:28:22 +1300

    add api to retrieve list of tokens

---
+++ src/server/controllers/tokens.controller.ts
@@ -0,0 +1,53 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+
+import {TokenModel} from "../models/token.model";
+import * as express from 'express';
+import Controller from "../../interfaces/controller.interface";
+import DataNotFoundException from "../../exceptions/DataNotFoundException";
+import NoDataException from "../../exceptions/NoDataExceptions";
+import authMiddleware from "../../middleware/auth.middleware";
+import SysEnv from "../../modules/SysEnv";
+import cors = require('cors');
+import { tokens_schema_table } from "../../schemas/tokens.schema";
+
+
+
+export class TokensController implements Controller{
+  public path='/tokens';
+  public router= express.Router();
+  private tokens = new TokenModel(tokens_schema_table);
+  siteCode = SysEnv.SITE_CODE;
+
+
+  constructor() {
+      this.siteCode = SysEnv.SITE_CODE;
+      this.intializeRoutes();
+  }
+
+  public intializeRoutes() {
+    this.router.get(this.path, cors(), authMiddleware, this.getAll);
+    this.router.get(this.path+'/byId/:id',  cors(), authMiddleware, this.findById);
+    return;
+  }
+
+
+  findById  = (request: express.Request, response: express.Response, next: express.NextFunction) => {
+    this.tokens.findById(request.params.id).then((respTokenDTO) => {
+      if (respTokenDTO) {
+        response.send(respTokenDTO.data);
+      } else {
+        next(new DataNotFoundException(request.params.id))
+      }
+    })
+  }
+
+  getAll  = (request: express.Request, response: express.Response, next: express.NextFunction) => {
+    this.tokens.getAll().then((respTokenDTOArray) => {
+      if (respTokenDTOArray) {
+        response.send(respTokenDTOArray);
+      } else {
+        next(new NoDataException())
+      }
+    })
+  }
+}
