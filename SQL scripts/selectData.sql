42ef236f06e708ddbbea92a59a1b8e8392410133
Brian Kuan <bkuan72@gmail.com>
2020-11-27 23:29:57 +1300

    add sql to list tokens and see the uuids

--- SQL scripts/selectData.sql
+++ SQL scripts/selectData.sql
@@ -1,13 +1,18 @@
 use testdb;
 
-select * from tokens;
-select * from blacklist_tokens;
+select BIN_TO_UUID(id), BIN_TO_UUID(uuid), BIN_TO_UUID(user_id) from tokens;
+select BIN_TO_UUID(id), BIN_TO_UUID(uuid), BIN_TO_UUID(user_id) from blacklist_tokens;
 select * from users;
 select * from user_accounts;
 select * from users;
 select * from logs;
 
-drop table userAccounts;
+
 delete from logs;
 delete from users;
+delete from tokens;
+delete from blacklist_tokens;
 
+drop table userAccounts;
+drop table tokens;
+drop table blacklist_tokens;
