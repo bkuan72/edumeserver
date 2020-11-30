4a82f9d70c7e63fe8f51eac2e82dc84c23277be0
Brian Kuan <bkuan72@gmail.com>
2020-12-01 09:43:41 +1300

    add new sqls

--- SQL scripts/selectData.sql
+++ SQL scripts/selectData.sql
@@ -3,16 +3,21 @@ use testdb;
 select BIN_TO_UUID(id), BIN_TO_UUID(uuid), BIN_TO_UUID(user_id) from tokens;
 select BIN_TO_UUID(id), BIN_TO_UUID(uuid), BIN_TO_UUID(user_id) from blacklist_tokens;
 select * from users;
+select * from accounts;
 select * from user_accounts;
 select * from users;
 select * from logs;
+select * from properties;
 
 
 delete from logs;
+delete from accounts;
+delete from user_accounts;
 delete from users;
 delete from tokens;
 delete from blacklist_tokens;
 
+drop table users;
 drop table userAccounts;
 drop table tokens;
 drop table blacklist_tokens;
