56bfc7b5f7bd1855b316559b1b84deb4ce767165
Brian Kuan <bkuan72@gmail.com>
2020-12-01 09:37:21 +1300

    new configuration for default admin user

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,5 +1,10 @@
 export const serverCfg =
 {
   crytoKey:"342148f4367bd36c2b0dd9dc975be6dd9f2518761d30d33644988bfbeb81b2d4",
-  bcryptSalt: 8
+  bcryptSalt: 8,
+  defaultAdminEmail: "edume@gmail.com",
+  defaultAdminUserId: "admin_edume",
+  defaultAdminUserName: "administrator",
+  defaultAdminPassword: "Admin123",
+  defaultAdminPhoneNo: "0211353221"
 }
