4ebc69ff0d88b9d35c4638f970a1ff4ee9a541e7
Brian Kuan <bkuan72@gmail.com>
2021-02-08 13:15:50 +1300

    remove obselete code

--- src/dtos/CreateUserDTO.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import CommonFn from '../modules/CommonFnModule';
-import DTOGenerator from '../modules/ModelGenerator';
-import { UserData, users_schema } from "../schemas/users.schema";
-
-
